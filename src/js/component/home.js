66da19874e32315983bd0f6b06067e958d3d9602
nitlara <nitlara@gmail.com>
2021-01-12 13:09:31 +0000

    aadded elements needed to load the components

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,15 +2,18 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { ToDoContainer } from "./todocointainer";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>todos</h1>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
+		<div className="container">
+			<div>
+				<h1>TODO</h1>
+			</div>
+			<div className="container">
+				<ToDoContainer />
+			</div>
 		</div>
 	);
 }
